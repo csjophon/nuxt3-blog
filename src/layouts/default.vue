276984261a10fa5918c6f2d69a21497e0c7e6674
jophon <1120659490@qq.com>
2025-07-02 22:26:30 +0800

    feat: change styles

--- src/layouts/default.vue
+++ src/layouts/default.vue
@@ -4,15 +4,14 @@
 <template>
   <div class="layout-default">
     <Navigation />
-    <div class="body">
-
+    <div class="layout-default-body">
 
       <main>
         <slot />
       </main>
 
     </div>
-    <div class="footer">
+    <div class="layout-default-footer">
       <div class="copyright">
         <span select-none>© Jophon 2024 - 2025</span>
         <!-- <a href="https://beian.miit.gov.cn/" target="_blank">粤ICP备2023134767号-2</a> -->
