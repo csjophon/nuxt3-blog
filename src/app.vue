50bd445fa03ba3a6b0b3d113496b48d18f38ab90
jophon <1120659490@qq.com>
2025-07-03 01:55:30 +0800

    feat: change styles

---
+++ src/app.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+        <header>
+            <Navigation />
+        </header>
+
+        <NuxtLayout>
+            <NuxtPage />
+        </NuxtLayout>
+
+        <footer>
+            <div class="footer">
+                <div class="copyright">
+                    <span select-none>© Jophon 2024 - 2025</span>
+                    <!-- <a href="https://beian.miit.gov.cn/" target="_blank">粤ICP备2023134767号-2</a> -->
+                </div>
+            </div>
+        </footer>
+    </div>
+
+</template>
--- src/layouts/default.vue
+++ src/layouts/default.vue
@@ -2,21 +2,9 @@
 </script>
 <template>
     <div>
-        <header>
-            <Navigation />
-        </header>
-
         <div class="body">
             <slot />
         </div>
-        <footer>
-            <div class="footer">
-                <div class="copyright">
-                    <span select-none>© Jophon 2024 - 2025</span>
-                    <!-- <a href="https://beian.miit.gov.cn/" target="_blank">粤ICP备2023134767号-2</a> -->
-                </div>
-            </div>
-        </footer>
     </div>
 
 </template>
